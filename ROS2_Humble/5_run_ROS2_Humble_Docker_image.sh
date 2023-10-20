aafd46ff5ddf4d7a84bed523824ce4811a958023
slowrunner <slowrunner@users.noreply.github.com>
2023-10-20 13:17:25 -0400

    fix for non-sudo invocation and net=host param for communication with distributed ROS 2

--- ROS2_Humble/5_run_ROS2_Humble_Docker_image.sh
+++ ROS2_Humble/5_run_ROS2_Humble_Docker_image.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# sudo docker run -it osrf/ros:humble-desktop
-echo
-echo *** PRESS CTRL-D TO EXIT DOCKER
-echo
-sudo docker run -it --net=host --privileged r2hd
+echo ""
+echo "*** PRESS CTRL-D or type exit TO EXIT DOCKER"
+echo ""
+docker run -it --net=host --privileged r2hd

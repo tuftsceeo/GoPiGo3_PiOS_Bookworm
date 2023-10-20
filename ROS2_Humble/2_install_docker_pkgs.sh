524a3748c06ff14aa63be4d1a37b1a007913d284
slowrunner <slowrunner@users.noreply.github.com>
2023-10-20 12:55:20 -0400

    add user to docker group to not need sudo to start docker

--- ROS2_Humble/2_install_docker_pkgs.sh
+++ ROS2_Humble/2_install_docker_pkgs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo docker run hello-world
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+newgrp docker
+docker run hello-world
 
 

cfb3da01a109dda9415b8cd2b3166fd0ddb7e109
slowrunner <slowrunner@users.noreply.github.com>
2023-10-21 21:57:51 -0400

    wrapped up ROS 2 in Docker on PiOS Bookworm - gopigo3 works, ROS 2 works, but ROS2 GoPiGo3 missing stuff to build successfully

--- ROS2_Humble/5_run_ROS2_Humble_Docker_image.sh
+++ ROS2_Humble/5_run_ROS2_Humble_Docker_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Docker invocation with a display
-#
+# -v /home:/home/pi/ \
 # -v `dirname $(pwd)`:/home/pi/bookworm_test/ROS2_Humble \
 # -v /etc/timezone:/etc/timezone:America/New_York
 # -e DISPLAY \
@@ -11,4 +11,6 @@
 echo ""
 echo "*** PRESS CTRL-D or type exit TO EXIT DOCKER"
 echo ""
-docker run -it --net=host --privileged r2hd
+docker run -it --net=host --privileged \
+-v `pwd`:`pwd` \
+ r2hd

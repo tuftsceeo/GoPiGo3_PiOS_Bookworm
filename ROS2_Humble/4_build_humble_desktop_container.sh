3ca55caefe1cf5cdc487be1b17f06f323b488fa7
slowrunner <slowrunner@users.noreply.github.com>
2024-08-10 10:28:42 -0400

    Delete ROS2_Humble directory

--- ROS2_Humble/4_build_humble_desktop_container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pushd ~/bookworm_test/ROS2_Humble/docker_images/ros/humble/ubuntu/jammy/desktop
-sudo docker build -t ros_docker .
-cp Dockerfile ~/bookworm_test/ROS2_Humble/ros2_humble_desktop_dockerfile
-popd
-sudo docker build . -t r2hd -f ros2_humble_desktop_dockerfile
--- ROS2_Humble/setups/2_install_docker_pkgs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo groupadd docker
-sudo usermod -aG docker ${USER}
-newgrp docker
-docker run hello-world
-
-

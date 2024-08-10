f9bf7f7647bb11dede1b536acc3dda4d840938a4
slowrunner <slowrunner@users.noreply.github.com>
2024-08-09 20:04:51 -0400

    Update Setup_PiOS_Bookworm_Desktop.sh

--- setups/Setup_PiOS_Bookworm_Desktop.sh
+++ setups/Setup_PiOS_Bookworm_Desktop.sh
@@ -12,17 +12,17 @@ sudo apt install -y --no-install-recommends python3-curtsies
 git clone https://github.com/DexterInd/DI_Sensors.git /home/pi/Dexter/DI_Sensors
 
 # === pigpiod
-wget https://github.com/joan2937/pigpio/archive/master.zip
-unzip master.zip
-cd pigpio-master
-make
-sudo make install
-cd ..
-rm master.zip
+# wget https://github.com/joan2937/pigpio/archive/master.zip
+# unzip master.zip
+# cd pigpio-master
+# make
+# sudo make install
+# cd ..
+# rm master.zip
 
-git clone https://github.com/slowrunner/bookworm_test.git /home/pi/bookworm_test
+git clone https://github.com/slowrunner/bookworm_test.git /home/pi/GoPiGo3_PiOS_Bookworm
 
-sudo cp /home/pi/bookworm_test/setups/pigpiod.service /etc/systemd/system
+sudo cp /home/pi/GoPiGo3_PiOS_Bookworm/setups/pigpiod.service /etc/systemd/system
 sudo systemctl enable pigpiod.service
 sudo systemctl start pigpiod.service
 systemctl status pigpiod.service
@@ -35,8 +35,8 @@ cd /home/pi/Dexter/lib/Dexter//RFR_Tools/miscellaneous/
 
 sudo mv di_i2c.py di_i2c.py.orig
 sudo mv setup.py setup.py.orig
-sudo cp ~/bookworm_test/i2c/di_i2c.py.bookworm di_i2c.py
-sudo cp ~/bookworm_test/RFR_Tools/setup.py .
+sudo cp ~/GoPiGo3_PiOS_Bookworm/i2c/di_i2c.py.bookworm di_i2c.py
+sudo cp ~/GoPiGo3_PiOS_Bookworm/RFR_Tools/setup.py .
 sudo python3 setup.py install
 
 # === also depends on smbus-cffi
@@ -58,26 +58,26 @@ systemctl status gpg3_power.service
 cd /home/pi/Dexter/GoPiGo3/Software/Python
 
 sudo mv setup.py setup.py.orig
-sudo cp ~/bookworm_test/GPG_Soft_Python/setup.py .
+sudo cp ~/GoPiGo3_PiOS_Bookworm/GPG_Soft_Python/setup.py .
 sudo python3 setup.py install
 
 cd /home/pi/Dexter/DI_Sensors/Python/di_sensors
 mv easy_distance_sensor.py easy_distance_sensor.py.orig
 mv distance_sensor.py distance_sensor.py.orig
-cp ~/bookworm_test/di_sensors/distance_sensor.py.bookworm distance_sensor.py
-cp ~/bookworm_test/di_sensors/easy_distance_sensor.py.bookworm easy_distance_sensor.py
+cp ~/GoPiGo3_PiOS_Bookworm/di_sensors/distance_sensor.py.bookworm distance_sensor.py
+cp ~/GoPiGo3_PiOS_Bookworm/di_sensors/easy_distance_sensor.py.bookworm easy_distance_sensor.py
 cd /home/pi/Dexter/DI_Sensors/Python
 sudo python3 setup.py install
 
 cd /home/pi/Dexter/GoPiGo3/Software/Python/Examples
 sudo mv easy_Distance_Sensor.py easy_Distance_Sensor.py.orig
-sudo cp ~/bookworm_test/Examples/easy_Distance_Sensor.py.bookworm easy_Distance_Sensor.py
+sudo cp ~/GoPiGo3_PiOS_Bookworm/Examples/easy_Distance_Sensor.py.bookworm easy_Distance_Sensor.py
 
 
 
 # ==== Setup non-root access rules ====
 
-sudo cp /home/pi/bookworm_test/setups/99-com.rules /etc/udev/rules.d
+sudo cp /home/pi/GoPiGo3_PiOS_Bookworm/setups/99-com.rules /etc/udev/rules.d
 
 cp /home/pi/Dexter/GoPiGo3/Install/list_of_serial_numbers.pkl /home/pi/Dexter/.list_of_serial_numbers.pkl
 
@@ -86,3 +86,13 @@ sudo apt install -y espeak-ng
 sudo pip3 install py-espeak-ng --break-system-packages
 espeak-ng "Am I alive? Can you hear me?"
 
+installresult=$(python3 -c "import gopigo3; g = gopigo3.GoPiGo3()" 2>&1)
+if [[ $installresult == *"ModuleNotFoundError"* ]]; then
+   echo "GOPIGO3 SOFTWARE INSTALLATION FAILURE: "+$installresult
+elif [[ $installresult == *"IOError"* ]]; then
+   echo "No SPI response. GoPiGo3 not detected: "+$installresult
+   echo "Ensure SPI is enabled in raspi-config."
+else
+    echo "GOPIGO3 SOFTWARE INSTALLATION SUCCESSFUL."
+    echo "Optional - Remove installation files: rm -rf ~/GoPiGo3_PiOS_Bookworm/"
+fi
